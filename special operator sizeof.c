b1b903694da252c0a0e18a808d1cef03622c2fbb
Shaik Rizwana <25kq1a5492@pace.ac.in>
2025-11-24 20:37:22 +0530

    Add files via upload

---
+++ special operator sizeof.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+      float f;
+      f=10.25;
+      printf("%d",sizeof(f));
+      
+    return 0;
+}
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    
