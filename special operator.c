261cab6132215e8f9b90776c3075859bf1c4cd11
Shaik Rizwana <25kq1a5492@pace.ac.in>
2025-11-24 20:27:41 +0530

    Add files via upload

---
+++ special operator.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+      int a,b,c;
+      c=(a=10,b=20,a+b);
+      printf ("%d",c);
+      
+    return 0;
+}
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    
