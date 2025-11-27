a2e7a93ae9ab6112f5cff522f04615d768329703
Shaik Rizwana <25kq1a5492@pace.ac.in>
2025-11-27 09:11:38 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   void add();
+   printf("hello");
+   printf ("\n I am working with");
+   printf("\n functions");
+   add();
+   printf("\n end the program");
+}
+void add()
+{
+  int a,b;
+  printf("\n enter a,b values:\n");
+  scanf("%d%d",&a,&b);
+  printf("%d",(a+b));
+}
+ 
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    
